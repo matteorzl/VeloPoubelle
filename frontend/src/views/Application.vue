3c81be947f084f82a714f170d244bf5ecdaccbe3
matteo.rzl <matteo.arzel@limayrac.fr>
2024-07-19 10:45:05 +0200

    fix: vue2 to vue3

--- frontend/src/views/Application.vue
+++ frontend/src/views/Application.vue
@@ -1,32 +1,26 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios';
+axios.defaults.baseURL = "http://localhost:3000"
+
+let user = ref(undefined)
+
+onMounted(() => {
+  axios.get('/api/app', {
+    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+  })
+  .then(res => {  
+    user.value = res.data.message;
+  })
+  .catch(error => {
+    console.error('Error fetching user data:', error);
+  });
+})
+</script>
+
 <template>
     <div>
-      <Sidebar :nom="user.nom" :prenom="user.prenom" />
-      <div  style="background-color: black !important; width: 900px; height: 1244px; margin-left: 25%; margin-top: 1%">
-   
-      </div>
+        <Sidebar :nom="user?.nom" :prenom="user?.prenom"/>
+        <div style="background-color: black !important; width: 900px; height: 1244px; margin-left: 25%; margin-top: 1%"></div>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        user: null 
-      };
-    },
-    mounted() {
-      axios.get('/api/app', {
-        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } // Include the JWT token
-      })
-      .then(res => {
-        this.user = res.data.message;
-      })
-      .catch(error => {
-        console.error('Error fetching user data:', error);
-        // Handle the error, e.g., redirect to login if the token is invalid
-      });
-    }
-  };
-  </script>
+</template>
